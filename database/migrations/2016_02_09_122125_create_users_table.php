d6fb44b630ce9f90deddd4d221c3535e6eaa7ce6
prodipto <prodip211085@gmail.com>
2016-02-09 18:18:47 +0530

    User Table Migration

--- database/migrations/2016_02_09_122125_create_users_table.php
+++ database/migrations/2016_02_09_122125_create_users_table.php
@@ -12,6 +12,7 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+        //
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
@@ -29,6 +30,7 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
+        //
         Schema::drop('users');
     }
 }
