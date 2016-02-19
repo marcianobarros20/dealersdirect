1f9aebcb82f2558f6a70ba8c1cdc06737bccdde3
prodipto <prodip211085@gmail.com>
2016-02-19 12:02:39 +0530

    Mail reconfigure

--- app/Http/Controllers/Front/AjaxController.php
+++ app/Http/Controllers/Front/AjaxController.php
@@ -12,6 +12,7 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Session;
 use Input;
+use Mail;
 use Illuminate\Support\Facades\Request;
 use Image\Image\ImageInterface;
 use Illuminate\Pagination\Paginator;
