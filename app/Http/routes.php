a97a84c4713735cd84081ddeaae6c1a70aa70f7f
prodipto <prodip211085@gmail.com>
2016-03-07 15:58:51 +0530

    Update Front Back And Api Funtionality

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -100,14 +100,14 @@ Route::group(
 |--------------------------------------------------------------------------
 |
 */
-Route::resource('/ajax/get_model', 'Front\AjaxController@getmodel');
-Route::resource('/ajax/get_year', 'Front\AjaxController@getyear');
-Route::resource('/ajax/requirment_queue', 'Front\AjaxController@requirmentqueue');
-Route::resource('/ajax/addstyletorequestqueue', 'Front\AjaxController@AddStyleToRequestqueue');
-Route::resource('/ajax/addenginetorequestqueue', 'Front\AjaxController@AddEngineToRequestqueue');
-Route::resource('/ajax/addtranstorequestqueue', 'Front\AjaxController@AddTransmissionToRequestqueue');
-Route::resource('/ajax/addexcolortorequestqueue', 'Front\AjaxController@AddExteriorColorToRequestqueue');
-Route::resource('/ajax/addincolortorequestqueue', 'Front\AjaxController@AddInteriorColorToRequestqueue');
+Route::post('/ajax/get_model', 'Front\AjaxController@getmodel');
+Route::post('/ajax/get_year', 'Front\AjaxController@getyear');
+Route::post('/ajax/requirment_queue', 'Front\AjaxController@requirmentqueue');
+Route::post('/ajax/addstyletorequestqueue', 'Front\AjaxController@AddStyleToRequestqueue');
+Route::post('/ajax/addenginetorequestqueue', 'Front\AjaxController@AddEngineToRequestqueue');
+Route::post('/ajax/addtranstorequestqueue', 'Front\AjaxController@AddTransmissionToRequestqueue');
+Route::post('/ajax/addexcolortorequestqueue', 'Front\AjaxController@AddExteriorColorToRequestqueue');
+Route::post('/ajax/addincolortorequestqueue', 'Front\AjaxController@AddInteriorColorToRequestqueue');
 
 /*
 |--------------------------------------------------------------------------
