0fa6a3de64a47bdfed5ccd816b49afb890350677
kingsuk <kingsuk.majumder@gmail.com>
2016-04-20 19:36:51 +0530

    changes in CSS

--- resources/views/front/section_includes/head.blade.php
+++ resources/views/front/section_includes/head.blade.php
@@ -3,7 +3,9 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="">
 	<meta name="author" content="">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>{{$title}}</title>
+
 
 	
 	
