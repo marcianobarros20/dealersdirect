dd0227e1b6f079ef3428bd1a4f6d8a2deaee2a46
prodipto <prodip211085@gmail.com>
2016-05-17 17:35:27 +0530

    email View Of Reminder Mail

--- resources/views/front/email/reminderleadmail.blade.php
+++ resources/views/front/email/reminderleadmail.blade.php
@@ -1,7 +1,7 @@
 <p style="font-size:20px;line-height:22px;color:#1588d1;font-family:'Lato', sans-serif;font-weight:700;display:block;text-align: left;margin:0;margin-bottom:20px;display:block;">Hey You Have A Reminder Mail</p>
 <p style="font-size:15px; line-height:20px;color:#4e4e4e;font-family:'Lato', sans-serif;font-weight:400;margin:0;margin-bottom:15px;display:block;text-align:left"> Reminder Note : {!!  $note !!}</p>
-<p style="font-size:15px; line-height:20px;color:#4e4e4e;font-family:'Lato', sans-serif;font-weight:400;margin:0;margin-bottom:15px;display:block;text-align:left"> Reminder Date {!! $rdate !!},</p>
-<p style="font-size:15px; line-height:20px;color:#4e4e4e;font-family:'Lato', sans-serif;font-weight:400;margin:0;margin-bottom:15px;display:block;text-align:left"> Reminder Time {!! $rdate !!},</p>
+<p style="font-size:15px; line-height:20px;color:#4e4e4e;font-family:'Lato', sans-serif;font-weight:400;margin:0;margin-bottom:15px;display:block;text-align:left"> Reminder Date {!! date("dS M Y", strtotime($rdate)) !!},</p>
+<p style="font-size:15px; line-height:20px;color:#4e4e4e;font-family:'Lato', sans-serif;font-weight:400;margin:0;margin-bottom:15px;display:block;text-align:left"> Reminder Time {!! date("h:i A", strtotime($rtime)) !!},</p>
 
 <p style="font-size:15px; line-height:20px;color:#4e4e4e;font-family:'Lato', sans-serif;font-weight:400;margin:0;margin-bottom:15px;display:block;text-align:left">Please click on the bellow link to View The Details.</p>
 
